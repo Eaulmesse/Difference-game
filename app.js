066940a960295be720efaef7f0d01f8679d7617e
Eaulmesse <theo.guerin01120@gmail.com>
2022-02-11 15:39:07 +0100

    feat: Adding the JS part

---
+++ app.js
@@ -0,0 +1,63 @@
+let affiche = document.getElementById("affiche");
+let barriere = document.getElementById("barriere");
+let basdegrille = document.getElementById("basdegrille");
+let fenetre = document.getElementById("fenetre");
+let graph = document.getElementById("graph");
+let lampe = document.getElementById("lampe");
+let marche = document.getElementById("marche");
+let portedegarage = document.getElementById("portedegarage");
+let toit = document.getElementById("toit");
+let mur = document.getElementById("mur");
+
+console.log(affiche);
+
+affiche.addEventListener("click",function(){
+    affiche.style.opacity="1";
+    affiche.style.transition="0.5s";
+});
+
+barriere.addEventListener("click",function(){
+    barriere.style.opacity="1";
+    barriere.style.transition="0.5s";
+});
+
+basdegrille.addEventListener("click",function(){
+    basdegrille.style.opacity="1";
+    basdegrille.style.transition="0.5s";
+});
+
+fenetre.addEventListener("click",function(){
+    fenetre.style.opacity="1";
+    fenetre.style.transition="0.5s";
+});
+
+graph.addEventListener("click",function(){
+    graph.style.opacity="1";
+    graph.style.transition="0.5s";
+});
+
+lampe.addEventListener("click",function(){
+    lampe.style.opacity="1";
+    lampe.style.transition="0.5s";
+});
+
+marche.addEventListener("click",function(){
+    marche.style.opacity="1";
+    marche.style.transition="0.5s";
+});
+
+portedegarage.addEventListener("click",function(){
+    portedegarage.style.opacity="1";
+    portedegarage.style.transition="0.5s";
+});
+
+toit.addEventListener("click",function(){
+    toit.style.opacity="1";
+    toit.style.transition="0.5s";
+});
+
+mur.addEventListener("click",function(){
+    mur.style.opacity="1";
+    mur.style.transition="0.5s";
+});
+
