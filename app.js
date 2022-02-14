9b80eb49d21c8bb43b733c32929d31316a008ec9
Eaulmesse <theo.guerin01120@gmail.com>
2022-02-14 14:06:35 +0100

    feat: Adding button for soluce/reset / winning animation and limit on the counter + some esthetic

--- app.js
+++ app.js
@@ -8,7 +8,59 @@ let marche = document.getElementById("marche");
 let portedegarage = document.getElementById("portedegarage");
 let toit = document.getElementById("toit");
 let mur = document.getElementById("mur");
+let soluce = [affiche, barriere , basdegrille, fenetre, graph, lampe, marche, portedegarage, toit, mur];
 let counterVal = 0;
+let containerSlot = document.querySelector(".slot")
+let emojis = ["💪","👌🏻","🏆","🎯"];
+
+console.log(soluce.length);
+
+function fiesta(){
+
+    if (counterVal == "10"){
+        for(let i = 0; i<50; i++){
+            let confetti = document.createElement("div");
+            confetti.innerText = emojis[Math.floor(Math.random()*emojis.length)];
+            containerSlot.appendChild(confetti);
+        }
+
+        animateConfettis();
+    }   
+}
+
+function animateConfettis(){
+
+
+    const TLCONF = gsap.timeline()
+
+    TLCONF
+    .to(".slot div",{
+        y: "random(-100,100)",
+        x: "random(-100,100)",
+        z: "random(0,1000)",
+        rotation: "random(-90,90)"
+    })
+    .to(".slot div", { autoAlpha: 0, duration: 2 }, "-=0.2")
+    .add(() => {
+        containerSlot.innerHTML = "";
+    });
+}
+
+function reveal(){
+    for(let i=0; i < soluce.length; i++) {
+      soluce[i].style.opacity = "1";  
+      soluce[i].style.transition = "0.5s";
+    }
+}
+
+function reset(){
+    for(let i=0; i < soluce.length; i++) {
+      soluce[i].style.opacity = "0";  
+      soluce[i].style.transition = "0.5s";
+      counterVal = 0;
+      updateDisplay(counterVal);
+    }
+}
 
 function updateDisplay(val) {
     console.log(val);
@@ -25,46 +77,46 @@ function opacityOne(param){
     param.style.opacity = "1";
     param.style.transition = "0.5s";
     incrementClick();
+    fiesta();
 }
 
-
-affiche.addEventListener("click",function(){
+affiche.addEventListener("click", function(){
     opacityOne(affiche);
-});
+}, {once : true});
 
 barriere.addEventListener("click",function(){
     opacityOne(barriere);
-});
+}, {once : true});
 
 basdegrille.addEventListener("click",function(){
     opacityOne(basdegrille);
-});
+}, {once : true});
 
 fenetre.addEventListener("click",function(){
     opacityOne(fenetre);
-});
+}, {once : true});
 
 graph.addEventListener("click",function(){
     opacityOne(graph);
-});
+}, {once : true});
 
 lampe.addEventListener("click",function(){
     opacityOne(lampe);
-});
+}, {once : true});
 
 marche.addEventListener("click",function(){
     opacityOne(marche);
-});
+}, {once : true});
 
 portedegarage.addEventListener("click",function(){
     opacityOne(portedegarage);
-});
+}, {once : true});
 
 toit.addEventListener("click",function(){
     opacityOne(toit);
-});
+}, {once : true});
 
 mur.addEventListener("click",function(){
     opacityOne(mur);
-});
+}, {once : true});
 
